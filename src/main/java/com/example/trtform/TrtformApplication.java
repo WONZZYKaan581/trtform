cc07ddfb642687ca21209b9da304647daf42a920
Kaan <kaan@Kaans-MacBook-Air.local>
2026-07-22 15:19:30 +0300

    İlk commit: Spring Boot iskeleti eklendi

---
+++ src/main/java/com/example/trtform/TrtformApplication.java
@@ -0,0 +1,13 @@
+package com.example.trtform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrtformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrtformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trtform/TrtformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trtform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrtformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
